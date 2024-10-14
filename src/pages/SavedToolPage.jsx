bf4abc62aa2cdf52d29b74b02e0fba3bcab5784e
minidev1228 <mightybluedev@gmail.com>
2024-10-15 01:18:08 +0900

    Update saved tools page

--- src/pages/SavedToolPage.jsx
+++ src/pages/SavedToolPage.jsx
@@ -6,8 +6,6 @@ import ToolCard from "../components/ToolCard";
 const SavedToolPage = () =>{
     const [tools, setTools] = useState([
         {img:"https://img.icons8.com/?size=100&id=106753&format=png&color=14203a", title:"Video downloader", isNew:true, num:5764},
-        {img:"https://img.icons8.com/?size=100&id=37326&format=png&color=14203a", title:"Youtube downloader", isNew:true, num:8887},
-        {img:"https://img.icons8.com/?size=100&id=8824&format=png&color=14203a", title:"Twitter downloader", isNew:false, num:8887},
         {img:"https://img.icons8.com/?size=100&id=GIWBO7Csvsvc&format=png&color=14203a", title:"Youtube to MP3", isNew:false, num:675},
         {img:"https://img.icons8.com/?size=100&id=9470&format=png&color=14203a", title:"Calculator", isNew:false, num:675},
     ])
