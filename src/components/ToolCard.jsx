f3fd9878a14d743e434a14671fd379d99d9f7978
minidev1228 <mightybluedev@gmail.com>
2024-10-15 14:51:29 +0900

    Remove eye icon on tool card

--- src/components/ToolCard.jsx
+++ src/components/ToolCard.jsx
@@ -7,10 +7,6 @@ const ToolCard = ({title, img, isNew, num}) =>{
                 isNew?<img className=" size-10 absolute right-5 top-5" src="https://img.icons8.com/?size=100&id=12423&format=png&color=0ea5e9" alt="" />:<></>
             }<img src={img} alt="" />
             <h3 className=" text-sm">{title}</h3>
-            <div className=" flex items-center justify-center absolute bottom-5 right-5">
-                <img className=" size-7 mr-2" src="https://img.icons8.com/?size=100&id=7877&format=png&color=14203a" alt="" />
-                <p>{num}</p>
-            </div>
         </div>
     )
 }
